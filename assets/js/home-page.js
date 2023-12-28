b57b1f960642856f8f66da5031bdce40ae9ba93d
AbhishekPolamarasetty <pashek321@gmail.com>
2023-12-28 18:01:38 +0530

    changed the home-page and getstarted page

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,3 +1,22 @@
+// const goTopBtn = document.querySelector('.go-top-btn');
+
+// window.addEventListener('scroll', checkHeight)
+
+// function checkHeight(){
+//   if(window.scrollY > 200) {
+//     goTopBtn.style.display = "flex"
+//   } else {
+//     goTopBtn.style.display = "none"
+//   }
+// }
+
+// goTopBtn.addEventListener('click', () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   })
+// })
+
 const goTopBtn = document.querySelector('.go-top-btn');
 
 window.addEventListener('scroll', checkHeight)
@@ -16,3 +35,10 @@ goTopBtn.addEventListener('click', () => {
     behavior: "smooth"
   })
 })
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
